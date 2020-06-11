0cd393951e8f0e63d3751542e2d5f67b2c2fae0d
Kayne <holykayne@gmail.com>
2020-06-11 22:43:56 +0200

    made notes about what was used and added comments.

--- CSI223_AssignmentOne_QuestionOne.sh
+++ CSI223_AssignmentOne_QuestionOne.sh
@@ -4,24 +4,24 @@
 # Karabo K. Tebelelo  201701145
 
 # Question One
-# To extract the sequence on line number 13th.
+# To extract the string on line number 13th.
 
-LineNumber13th=$(sed -n 14p Corona.fasta)
+LineNumber13th=$(sed -n 14p Corona.fasta) # '-n' suppress automatic printing of pattern sequence, '14p' means the 14 line which is the 13th line since we ignore the first line.
 echo "The 13th line is $LineNumber13th"
 
 # To count the number of lines in the file "corona".
 
-NumberOfLines=$(awk '!/>/{print }' Corona.fasta | tail -n +1 | wc -l)
+NumberOfLines=$(awk '{print }' Corona.fasta | tail -n +2 | wc -l) # "-l" print newline counts, '-n +1' print the last lines instead of 10 lines starting from line number 1.
 echo "Total number of lines in the file is $NumberOfLines"
 
 # To count the number of characters in the file.
 
-NumberOfCharacters=$(awk '!/>/{print }' Corona.fasta | wc -c)
+NumberOfCharacters=$(awk '{print }' Corona.fasta | tail -n +1 | wc -m) # "-m" prints character counts, '{print}' print the output.
 echo "The total number of characters is $NumberOfCharacters"
 
 # To count only 'T' in the file.
 
-NumberOfTs=$( grep -o T Corona.fasta | wc -l)
+NumberOfTs=$( grep -o T Corona.fasta | wc -l) # 'grep -o' print the only match from a matched line.
 echo "Total number of T's is $NumberOfTs"
 
 # To count the number of times symbols occur.
